12f4184d65c248c067f144ddeed233e844c0fc91
danmj <daniel.jung@stern.nyu.edu>
2018-08-14 17:46:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(x)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif x == x.upcase
     puts "NO, NOT SINCE 1938!"
-    return 
+    return "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
